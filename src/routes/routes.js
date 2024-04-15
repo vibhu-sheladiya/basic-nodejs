e18dbb0afee67f01545a20f11dc88b6c2d933c06
vibhu-sheladiya <vibhasheladiya3936@gmail.com>
2024-04-15 17:13:46 +0400

    initally

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const { register, login, updateUser,fetchList,deleteUser } = require("../controllers/user");
+const { register, login, updateUser,fetchList,deleteUser,searchApi,pagination,
+    // fetchListAllSearchPage
+ } = require("../controllers/user");
 const { userAuth } = require("../middleware/auth");
 const router = express();
 
@@ -7,6 +9,13 @@ router.post("/create", register);
 router.post("/login", userAuth(), login);
 router.put("/update", updateUser);
 router.get('/list',userAuth(),fetchList)
+router.get('/search',searchApi)
+router.get('/page',pagination)
+// router.get('/list-search',fetchListAllSearchPage)
+
+
+
+
 router.delete('/delete',deleteUser)
 
 
